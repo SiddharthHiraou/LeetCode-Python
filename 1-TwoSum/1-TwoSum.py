1a6b5b7d8a33860d4ea00062914b36ac03238fd0
Siddharth_Hiraou <89652594+SiddharthHiraou@users.noreply.github.com>
2025-07-06 00:05:46 +0530

    File updated at 7/6/2025, 12:05:45 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,25 +1,11 @@
+# Last updated: 7/6/2025, 12:05:45 AM
 class Solution(object):
-    def combinationSum2(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        res = []
-        candidates.sort()
-        self.dfs(candidates, target, [], res)
-        return res
-    
-    
-    def dfs(self, candidates, target, path, res):
-        if target < 0:
-            return
+    def twoSum(self, nums, target):
+        ans={}
+        for i,num in enumerate(nums):
+            comp=target-num
+            if comp in ans:
+                return[ans[comp],i]
+            ans[num]=i
+            
         
-        if target == 0:
-            res.append(path)
-            return res
-        
-        for i in range(len(candidates)):
-            if i > 0 and candidates[i] == candidates[i-1]: #1
-                continue #2
-            self.dfs(candidates[i+1:], target - candidates[i], path+[candidates[i]], res) #3
