a664cd6b73de3b40b0673170df525d069c8ff44d
Siddharth_Hiraou <89652594+SiddharthHiraou@users.noreply.github.com>
2025-07-06 00:46:12 +0530

    File updated at 7/6/2025, 12:46:12 AM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,8 +1,10 @@
+# Last updated: 7/6/2025, 12:46:12 AM
 class Solution(object):
     def groupAnagrams(self, strs):
         ans=defaultdict(list)
-        for ele in strs:
-            sorted_word=''.join(sorted(ele))
-            ans[sorted_word].append(ele)
+        for i in strs:
+            key=''.join(sorted(i))
+            ans[key].append(i)
         return list(ans.values())
+
         
