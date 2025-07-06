b68502bffaca9a96d0a7ed633688f2476ae2f4e4
Siddharth_Hiraou <89652594+SiddharthHiraou@users.noreply.github.com>
2025-07-06 16:23:44 +0530

    Add solution for 347. Top K Frequent Elements

---
+++ medium/347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq_dic={}
+        for i in nums:
+            if i in freq_dic:
+                freq_dic[i]+=1
+                continue
+            freq_dic[i]=1;
+            
+        topk= sorted(freq_dic, key = freq_dic.get, reverse=True)[:k]
+        return topk
+        
