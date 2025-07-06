d3130b35121a8f02b3ab24f704331821a20e938d
Siddharth_Hiraou <89652594+SiddharthHiraou@users.noreply.github.com>
2025-07-07 00:58:15 +0530

    File updated at 7/7/2025, 12:58:15 AM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,14 +1,14 @@
+# Last updated: 7/7/2025, 12:58:15 AM
 class Solution(object):
     def productExceptSelf(self, nums):
-        n=len(nums)
+        n = len(nums)
         ans=[1]*n
-        prefix=1
+        left = 1
         for i in range(n):
-            ans[i]=prefix
-            prefix*=nums[i]
-        suffix=1
+            ans[i]=left
+            left*=nums[i]
+        right=1
         for i in reversed(range(n)):
-            ans[i]*=suffix
-            suffix*=nums[i]
+            ans[i]*=right
+            right*=nums[i]
         return ans
-        
