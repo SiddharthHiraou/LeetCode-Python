e5b6e99f6380c506fb029053c2329ff36a89036d
Siddharth_Hiraou <89652594+SiddharthHiraou@users.noreply.github.com>
2025-07-22 00:55:49 +0530

    Add solution for 167. Two Sum II - Input Array Is Sorted

---
+++ medium/167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,11 @@
+        left=0
+        right=len(numbers)-1
+ 
+        while(left<right):
+            cur_sum= numbers[left]+numbers[right]
+            if cur_sum==target:
+                return[left+1,right+1]
+            elif cur_sum<target:
+                left+=1
+            else:
+                right-=1
