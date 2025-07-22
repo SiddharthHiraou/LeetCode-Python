e35a5f7f9a56970fa7797d5a678bf81ae1687e42
Siddharth_Hiraou <89652594+SiddharthHiraou@users.noreply.github.com>
2025-07-23 00:30:16 +0530

    File updated at 7/23/2025, 12:30:16 AM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,18 +1,31 @@
+# Last updated: 7/23/2025, 12:30:16 AM
 class Solution(object):
     def threeSum(self, nums):
-        nums.sort() 
-        res = set()
-        for i in range(len(nums)):
-            target = -nums[i] 
-            seen = set()
-        
-            for j in range(i + 1, len(nums)):
-                complement = target - nums[j]
+        nums.sort()
+        ans=[]
+        for i,a in enumerate(nums):
+            if i>0 and a==nums[i-1]:
+                continue
             
-                if complement in seen:
-                    res.add((nums[i], complement, nums[j]))
+            l,r=i+1,len(nums)-1
+            while(l<r):
+                threesum= a+nums[l]+nums[r]
+                if threesum==0:
+                    ans.append([nums[i],nums[l],nums[r]])
+
+                    while l<r and nums[l]==nums[l+1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r-1]:
+                        r-=1
+                    l+=1
+                    r-=1
+
+                elif threesum<0:
+                    l+=1
                 else:
-                    seen.add(nums[j])
-    
-        return [list(triplet) for triplet in res]
+                    r-=1
+                
+        return ans
+
+
         
